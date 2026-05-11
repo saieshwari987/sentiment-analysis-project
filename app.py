03a763413d5568d79a0c67bed02e17b95bd02065
Sai <01fe24ica044@kletech.ac.in>
2026-05-11 19:07:28 +0530

    fixed render port

--- app.py
+++ app.py
@@ -102,5 +102,8 @@ def upload():
                     top_positive=top_positive,
                     top_negative=top_negative)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
